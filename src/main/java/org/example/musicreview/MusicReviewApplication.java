4d2a6099e913fa3183761a8d08680305302f2353
Karthick Govindaraj <136472779+Karthick-Govindaraj007@users.noreply.github.com>
2024-06-10 21:47:49 -0400

    Initial Commit, Basic Project

---
+++ src/main/java/org/example/musicreview/MusicReviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.musicreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/musicreview/MusicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musicreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
