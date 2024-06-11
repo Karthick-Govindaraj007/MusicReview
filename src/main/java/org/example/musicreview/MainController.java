f93e6ad52f7c9bec8a662f4ec048dc17413f09d3
Karthick Govindaraj <136472779+Karthick-Govindaraj007@users.noreply.github.com>
2024-06-10 22:28:21 -0400

    Added routing in java, changed html hrefs to match the java routing

--- src/main/java/org/example/musicreview/MainController.java
+++ src/main/java/org/example/musicreview/MainController.java
@@ -7,8 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/search-results")
+    public String searchResults() {
+        return "search-results";
+    }
+
+    @GetMapping("/about-us")
+    public String aboutUs() {
+        return "about-us";
+    }
+
+    @GetMapping("/future-updates")
+    public String futureUpdates() {
+        return "future-updates";
     }
 
     @GetMapping("/error")
